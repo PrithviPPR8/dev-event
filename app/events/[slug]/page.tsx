75365e9cc37a9ac21ff357f4203f52e1ff1a0958
Prithviraj Patil <prithvirajpatil64@gmail.com>
2025-12-06 10:28:05 +0530

    Server Actions done

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,4 +1,7 @@
 import BookEvent from "@/components/BookEvent";
+import EventCard from "@/components/EventCard";
+import { IEvent } from "@/database";
+import { getSimilarEventsBySlug } from "@/lib/actions/event.actions";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
@@ -51,6 +54,10 @@ const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }>}
 
   const bookings = 10;
 
+  const similarEvents: IEvent[] = await getSimilarEventsBySlug(slug);
+
+  console.log(similarEvents);
+
   return (
     <section id="event">
       <div className="header">
@@ -84,14 +91,16 @@ const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }>}
             <EventDetailItem icon="/icons/audience.svg" alt="audience" label={audience} />
           </section>
 
-          <EventAgenda agendaItems={JSON.parse(agenda[0])} />
+          {/* <EventAgenda agendaItems={JSON.parse(agenda[0])} /> */}
+          <EventAgenda agendaItems={agenda} />
 
           <section className="flex-col-gap-2">
             <h2>About the Organizer</h2>
             <p>{organizer}</p>
           </section>
 
-          <EventTags tags={JSON.parse(tags[0])} />
+          {/* <EventTags tags={JSON.parse(tags[0])} /> */}
+          <EventTags tags={tags} />
         </div>
 
         {/* Right Side - Booking Form */}
@@ -109,6 +118,15 @@ const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }>}
             <BookEvent />
           </div>
         </aside>
+      </div>
+
+      <div className="flex w-full flex-col gap-4 pt-20">
+        <h2>Similar Events</h2>
+        <div className="events">
+          {similarEvents.length > 0 && similarEvents.map((similarEvent: IEvent) => (
+            <EventCard key={similarEvent.title} {...similarEvent } />
+          ))}
+        </div>
       </div>
     </section>
   )
