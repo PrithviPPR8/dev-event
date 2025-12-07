d3ac3b136910e29fecf73a6186b41c2c73b8625c
Prithviraj Patil <prithvirajpatil64@gmail.com>
2025-12-07 10:23:58 +0530

    Caching removed

--- components/EventDetails.tsx
+++ components/EventDetails.tsx
@@ -36,8 +36,8 @@ const EventTags = ({ tags }: { tags: string[] }) => (
 )
 
 const EventDetails = async ({ params }: { params: Promise<string> }) => {
-    'use cache'
-    cacheLife('hours');
+    // 'use cache'
+    // cacheLife('hours');
     const slug = await params;
 
     let event;
